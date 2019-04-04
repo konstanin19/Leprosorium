8649fce8da9e6355cf65a5cc33d56c9d22b7bd31
Konstantin K <konstanin19@mail.ru>
2019-04-04 22:49:19 +0300

    Save content to database

--- app.rb
+++ app.rb
@@ -38,5 +38,6 @@ post '/new' do
 		@error = 'Type post text'
 		return erb :new
 	end
+	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 	erb "You typed: #{content}"
 end
