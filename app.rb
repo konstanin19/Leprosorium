3341c702e4804bdcbefe508a012f2d5f83ea77d5
Konstantin K <konstanin19@mail.ru>
2019-04-05 23:37:44 +0300

    Select comments to @comments variable

--- app.rb
+++ app.rb
@@ -59,6 +59,8 @@ get '/details/:post_id' do
 
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
 	@row = results[0]
+
+	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
 	erb :details
 end
 
